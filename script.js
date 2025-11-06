47d4094052ee92d8359eeecebee73edfc0f98021
adarsh-devx <adarshkushwaha0009@gmail.com>
2025-11-06 21:53:29 +0530

    ui changes

--- script.js
+++ script.js
@@ -11,3 +11,35 @@ if (customCursor) {
         customCursor.classList.remove('clicking');
     });
 }
+
+const h1 = document.querySelector('h1');
+if (h1) {
+    const text = h1.textContent;
+    const newText = text.split('').map(char => `<span>${char}</span>`).join('');
+    h1.innerHTML = newText;
+
+    h1.addEventListener('mouseover', function(event) {
+        if (event.target.tagName === 'SPAN') {
+            event.target.classList.add('hovered');
+        }
+    });
+
+    h1.addEventListener('mouseout', function(event) {
+        if (event.target.tagName === 'SPAN') {
+            event.target.classList.remove('hovered');
+        }
+    });
+
+    h1.addEventListener('touchstart', function(event) {
+        if (event.target.tagName === 'SPAN') {
+            event.target.classList.add('hovered');
+        }
+    });
+
+    h1.addEventListener('touchend', function(event) {
+        if (event.target.tagName === 'SPAN') {
+            event.target.classList.remove('hovered');
+        }
+    });
+}
+
